6db8ed5a9a02a3d4e2bf9ec5876e89723f9374c8
B A Y R O N <bayron_sanchez@unitec.edu>
2025-09-12 13:33:47 -0600

    subiendo cambios para siwtch de notificaciones

--- proyectoMovil/App.tsx
+++ proyectoMovil/App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ButtonTabNavegador from './Componentes/ButtonTabNavegacion';
 import ProviderEvento from './Providers/ProviderEvento';
+import * as Notifications from "expo-notifications";
 
 export default function App() {
   return (
